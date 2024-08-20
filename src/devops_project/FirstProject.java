165a6c41e8d1307ecf1b1d48da09d13030c46e32
AdmiN <AdmiN@ITWS0019>
2024-08-20 10:17:42 +0530

    commit

---
+++ src/devops_project/FirstProject.java
@@ -0,0 +1,11 @@
+package devops_project;
+
+public class FirstProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Devops Lab");
+
+	}
+
+}
